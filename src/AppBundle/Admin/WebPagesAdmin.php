1fff4d4a6f3963f6f489046c78b498ee8e878fbe
Win10 <ruben.petrosan@actency.fr>
2017-11-12 23:29:57 +0100

    Creating Symfony Sonata Admin Bundle
       ---- 2 Entity ------
          WebPages + Twigs
    
          WebPages
             - Twigs -> entity Twigs
             - TitlePage (string) 255 char
             - KeyWords (string) 255 char
             - Description (string) 255 char
    
         Twigs
            - TwigName (string) 255 char
            - TwigCss (string) 255 char
            - TewigJs (string) 255 char

---
+++ src/AppBundle/Admin/WebPagesAdmin.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Win10
+ * Date: 12.11.2017
+ * Time: 21:06
+ */
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+
+class WebPagesAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('pageTitle', 'text')
+            ->add('twigs','entity',
+                array(
+                    'class' => 'AppBundle\Entity\Twigs',
+                    'choice_label' => 'Twig Name',
+                    'multiple' => true,
+                )
+            )
+            ->add('pageKeyWord', 'text', array(
+
+                'required' => false,
+                'empty_data' => '',
+
+            ))
+            ->add('pageDescription', 'text', array(
+
+                'required' => false,
+                'empty_data' => '',
+
+            ))
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('pageTitle');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('pageTitle')
+            ->addIdentifier('pageKeyWord')
+            ->addIdentifier('pageDescription')
+        ;
+
+    }
+}
